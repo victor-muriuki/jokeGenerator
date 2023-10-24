cd7ad7355a252ea7e74821505dacdbb77bffab70
+ <+>
2023-10-24 13:05:20 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded',
+function(){
+    // fetch('https://dad-jokes.p.rapidapi.com/joke/5f80ccd641785ba7c7d27b66',)
+    // .then(res => res=JSON)
+    // .then(data => {
+    //     const filmData = data
+    //     renderFilms(filmData))
+    
+})
+
+
